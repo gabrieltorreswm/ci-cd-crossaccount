d33a9b60f164a5a5260777e81d3267213bf03d1e
Gabriel <gabrieltorreswm@gmail.com>
2023-08-27 15:15:29 -0500

    Initial commit

---
+++ test/ci-cd-crossaccount.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CiCdCrossaccount = require('../lib/ci-cd-crossaccount-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ci-cd-crossaccount-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CiCdCrossaccount.CiCdCrossaccountStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
